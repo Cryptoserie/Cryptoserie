0ab3fee64fd201a94b6b001722abecb3fcc46e41
Cryptoserie <97411800+Cryptoserie@users.noreply.github.com>
2022-09-14 19:22:51 +0100

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -136,8 +136,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <footer className="site-foot">
                     <div className="site-foot-nav container">
                         <div className="site-foot-nav-left">
-                            <Link to="/">{site.title}</Link> © 2022 | 
-                            Par
+                            <Link to="/">{site.title}</Link> © 2022 |
                             <a
                                 className="site-foot-nav-item"
                                 href="https://www.akselinc.com"
